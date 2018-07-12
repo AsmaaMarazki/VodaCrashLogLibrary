6f5884ac8ceafe6400e91cd43a48c5289d55bd30
AsmaaMarazki <asmaa.marazki@gmail.com>
2018-07-12 09:41:48 +0200

    Vodalytics instance.

--- app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/lib/Vodalytics.java
+++ app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/lib/Vodalytics.java
@@ -1,4 +1,33 @@
 package com.example.asmaamarazki.vodacrashloglibrary.lib;
 
 public class Vodalytics {
+    private static volatile Vodalytics vodalytics;
+
+    private Vodalytics(){
+        if(vodalytics !=null)
+            throw new RuntimeException("Use getInstance() method to get the single instance of the class.");
+    }
+
+    public static Vodalytics getInstance(){
+        if (vodalytics==null) {
+            synchronized (Vodalytics.class){
+                if (vodalytics==null)
+                vodalytics = new Vodalytics();
+            }
+        }
+        return vodalytics;
+    }
+
+    public static void log(Throwable throwable){
+
+    }
+    public static void log(String msg){
+
+    }
+    public static void log(String key,String msg){
+
+    }
+
+
 }
+
