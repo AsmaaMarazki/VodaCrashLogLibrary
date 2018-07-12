01d56a3d1d09bbac51a277070c9799e8e1e22693
Yomna Gamal <yomna.g.hussein@gmail.com>
2018-07-12 11:02:35 +0200

    RoomDataBase

--- app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/MainActivity.java
+++ app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.asmaamarazki.vodacrashloglibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.asmaamarazki.vodacrashloglibrary.lib.database.DataSource;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
