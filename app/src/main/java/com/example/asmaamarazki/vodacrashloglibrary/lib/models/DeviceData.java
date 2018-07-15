ca1e39160069ccd3d523c7243b06275687790d41
Yomna Gamal <yomna.g.hussein@gmail.com>
2018-07-15 12:22:18 +0200

    model classes v2

--- app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/lib/models/DeviceData.java
+++ app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/lib/models/DeviceData.java
@@ -1,4 +1,40 @@
 package com.example.asmaamarazki.vodacrashloglibrary.lib.models;
 
 public class DeviceData {
+    private boolean isWifiConnected;
+    private boolean isMobileDataConnected;
+    private String deviceType;
+    private String deviceOsVersion;
+
+    public boolean isWifiConnected() {
+        return isWifiConnected;
+    }
+
+    public void setWifiConnected(boolean wifiConnected) {
+        isWifiConnected = wifiConnected;
+    }
+
+    public boolean isMobileDataConnected() {
+        return isMobileDataConnected;
+    }
+
+    public void setMobileDataConnected(boolean mobileDataConnected) {
+        isMobileDataConnected = mobileDataConnected;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceOsVersion() {
+        return deviceOsVersion;
+    }
+
+    public void setDeviceOsVersion(String deviceOsVersion) {
+        this.deviceOsVersion = deviceOsVersion;
+    }
 }
--- app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/lib/models/UserData.java
+++ app/src/main/java/com/example/asmaamarazki/vodacrashloglibrary/lib/models/UserData.java
@@ -1,6 +1,59 @@
 package com.example.asmaamarazki.vodacrashloglibrary.lib.models;
 
 public class UserData {
-    boolean isSeamless;
-    String msisdn;
+
+    private boolean isSeamless;
+    private String msisdn;
+    private String[] msisdnAllAccounts;
+    private String ratePlanCode;
+    private String tireId;
+    private String[] userRoles;
+
+    public boolean isSeamless() {
+        return isSeamless;
+    }
+
+    public void setSeamless(boolean seamless) {
+        isSeamless = seamless;
+    }
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public String[] getMsisdnAllAccounts() {
+        return msisdnAllAccounts;
+    }
+
+    public void setMsisdnAllAccounts(String[] msisdnAllAccounts) {
+        this.msisdnAllAccounts = msisdnAllAccounts;
+    }
+
+    public String getRatePlanCode() {
+        return ratePlanCode;
+    }
+
+    public void setRatePlanCode(String ratePlanCode) {
+        this.ratePlanCode = ratePlanCode;
+    }
+
+    public String getTireId() {
+        return tireId;
+    }
+
+    public void setTireId(String tireId) {
+        this.tireId = tireId;
+    }
+
+    public String[] getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(String[] userRoles) {
+        this.userRoles = userRoles;
+    }
 }
